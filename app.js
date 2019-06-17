d58bf8bea821e1802bbb2c8d886b5f486f2e9ca9
Noob Scorpio <aliadam7867@gmail.com>
2019-06-17 21:01:10 +0500

    newBranch test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-console.log("ALright!!!");
+console.log("ALright! 2222!!");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
